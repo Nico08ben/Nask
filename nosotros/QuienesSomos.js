b1c88d10b6b9ace5c93d975b905583584c87ab3c
Nicolas <armeronico2008@gmail.com>
2024-04-03 17:58:29 -0500

    Añadida parte de Kevin y Alejandro

---
+++ nosotros/QuienesSomos.js
@@ -0,0 +1,19 @@
+const typed = new Typed ('.typed', {
+    strings: [
+    '<i class="verb">QUIENES SOMOS</i>',
+    '<i class="verb">QUE CONSTRUIREMOS</i>',
+    '<i class="verb">DE DONDE VENIMOS</i>',
+    '<i class="verb">NUESTRA VISION</i>'],
+
+    TypeSpeed: 75,
+    startDelay: 500,
+    backSpeed: 75,
+    smartBackspace: true,
+    shuffle: false,
+    backDelay: 1500,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: '|',
+    contentType: 'html',
+});
